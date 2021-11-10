071f35392655451bb502d1d92a228441f09f469f
DeeAnnEye <danielashulamithjustin@gmail.com>
2021-11-10 09:49:15 +0530

    Add advertiser details

--- src/components/AdvertiserDetails.js
+++ src/components/AdvertiserDetails.js
@@ -1,16 +1,25 @@
 import React from "react";
 
-const DetailsTable = () =>{
-return<div class="bg-white min-w-1/2 shadow overflow-hidden sm:rounded-lg">
-<div class="px-4 py-5 sm:px-6">
-  <h6 class="text-left leading-6 font-medium text-gray-800">
+const AdvertiserDetails = () =>{
+return <div class="bg-white max-w-2xl min-w-1/2 shadow overflow-hidden sm:rounded-lg">
+<div class=" flex justify-between px-4 py-5 sm:px-6">
+  <div class="text-left text-xl text-gray-800">
     Details
-  </h6>
-  {/* <button type="button" class="inline-flex items-center justify-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-blue-500">
+  </div>
+  <div>
+  <button type="button" class="inline-flex items-center justify-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-blue-500">
     Edit
-  </button> */}
+  </button>
+  </div>
 </div>
 <div class="border-t border-gray-200 px-4 py-5 sm:px-6">
+    <div class="text-left font-small text-gray-400">
+        Address:
+    </div>
+    <div class="mt-1 text-left text-gray-600">
+    Fugiat ipsum ipsum deserunt culpa aute sint do nostrud anim incididunt cillum culpa consequat.
+    Excepteur qui ipsum aliquip consequat sint. Sit id mollit nulla mollit nostrud in ea
+    </div>
   <dl class="grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2">
     <div class="sm:col-span-1">
       <dt class="text-left font-small text-gray-400">
@@ -81,4 +90,4 @@ return<div class="bg-white min-w-1/2 shadow overflow-hidden sm:rounded-lg">
 </div>
 }
 
-export default DetailsTable
+export default AdvertiserDetails
--- src/pages/Advertiser.js
+++ src/pages/Advertiser.js
@@ -1,6 +1,8 @@
 import React from "react";
 import AdvertiserPageBar from "../components/AdvertiserPageBar";
 import AdvertiserProfile from "../components/AdvertiserProfile";
+import AdvertiserDetails from "../components/AdvertiserDetails"
+import UserTable from "../components/UserTable"
 import NavBar from "../components/NavBar";
 
 const Advertiser = () =>{
@@ -8,6 +10,10 @@ return <div class="min-h-full">
     <NavBar />
     <AdvertiserPageBar />
     <AdvertiserProfile />
+    <div className=" flex flex-row px-4 gap-4 py-4 flex-wrap bg-white shadow">
+    <AdvertiserDetails />
+    <UserTable />
+  </div>
 </div>
 }
 
