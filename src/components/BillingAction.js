8988b1979e688c516a9258f9857f4e805dc7781d
DeeAnnEye <danielashulamithjustin@gmail.com>
2021-11-14 12:48:16 +0530

    Change billing action

--- src/components/BillingAction.js
+++ src/components/BillingAction.js
@@ -53,21 +53,21 @@ const BillingAction = () => {
                   type="text"
                   name="currency"
                   id="currency"
-                  placeholder="USD"
+                  placeholder="ABCDEFGH"
                   class="shadow-sm h-10 focus:ring-indigo-500 focus:border-indigo-500 pl-2 block w-60 text-base border border-gray-400 rounded-md"
                 />
               </div>
             </dd>
           </div>
           <div class="sm:col-span-1">
-            <dt class="text-left font-small text-gray-500">Card Name:</dt>
+            <dt class="text-left font-small text-gray-500">Card Number:</dt>
             <dd class="mt-1 text-left text-gray-900">
               <div class="mt-1">
                 <input
                   type="text"
                   name="currency"
                   id="currency"
-                  placeholder="USD"
+                  placeholder="55255128552"
                   class="shadow-sm h-10 focus:ring-indigo-500 focus:border-indigo-500 pl-2 block w-60 text-base border border-gray-400 rounded-md"
                 />
               </div>
@@ -83,7 +83,48 @@ const BillingAction = () => {
                   type="text"
                   name="currency"
                   id="currency"
-                  placeholder="USD"
+                  placeholder="Address"
+                  class="shadow-sm h-10 focus:ring-indigo-500 focus:border-indigo-500 pl-2 block w-60 text-base border border-gray-400 rounded-md"
+                />
+              </div>
+            </dd>
+          </div>
+          <div class="sm:col-span-1">
+            <dt class="text-left font-small text-gray-500">Expiry:</dt>
+            <dd class="mt-1 text-left text-gray-900">
+              <div class="mt-1">
+                <input
+                  type="text"
+                  name="currency"
+                  id="currency"
+                  placeholder="--/--"
+                  class="shadow-sm h-10 focus:ring-indigo-500 focus:border-indigo-500 pl-2 block w-60 text-base border border-gray-400 rounded-md"
+                />
+              </div>
+            </dd>
+          </div>
+          <div class="sm:col-span-1">
+          <dt class="text-left font-small mb-8 text-gray-500"></dt>
+            <dd class="mt-1 text-left text-gray-900">
+              <div class="mt-1">
+                <button
+                  type="button"
+                  class="inline-flex items-center px-5 py-4 border border-gray-300 shadow-sm text-lg leading-4 font-medium rounded-md text-blue-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                  ADD
+                </button>
+              </div>
+            </dd>
+          </div>
+          <div class="sm:col-span-1">
+            <dt class="text-left font-small text-gray-500">CVV:</dt>
+            <dd class="mt-1 text-left text-gray-900">
+              <div class="mt-1">
+                <input
+                  type="text"
+                  name="currency"
+                  id="currency"
+                  placeholder="234"
                   class="shadow-sm h-10 focus:ring-indigo-500 focus:border-indigo-500 pl-2 block w-60 text-base border border-gray-400 rounded-md"
                 />
               </div>
@@ -91,6 +132,151 @@ const BillingAction = () => {
           </div>
         </dl>
       </div>
+      <div class="shadow mx-8 my-2 w-max overflow-hidden border border-gray-200 sm:rounded-lg">
+              <table class="divide-y divide-gray-200">
+                <thead class="bg-gray-50">
+                  <tr>
+                    <th
+                      scope="col"
+                      class="px-10 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      CARD <br/>
+                      NUMBER
+                    </th>
+                    <th
+                      scope="col"
+                      class="px-10 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      NAME
+                    </th>
+                    <th
+                      scope="col"
+                      class="px-10 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      EXPIRY
+                    </th>
+                    <th
+                      scope="col"
+                      class="px-10 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      APPLY TO
+                    </th>
+                    <th
+                      scope="col"
+                      class="px-10 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      ACTION
+                    </th>
+                  </tr>
+                </thead>
+                <tbody class="bg-white divide-y divide-gray-200">
+                  <tr>
+                    <td class="px-6 py-4 whitespace-nowrap">
+                      <div class="flex items-center">
+                        <div class="flex-shrink-0">235</div>
+                      </div>
+                    </td>
+                    <td class="px-6 py-4 whitespace-nowrap">
+                      <div class="text-sm text-gray-500">ABC</div>
+                    </td>
+                    <td class="px-6 py-4 whitespace-nowrap">
+                      <div class="text-sm text-gray-500">
+                        --/--
+                      </div>
+                    </td>
+                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                    <button type="button" class="inline-flex items-center px-3 py-2 border border-gray-300 shadow-sm text-sm leading-4 font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                      Set as Default
+                    </button>
+                    </td>
+                    <td class="px-6 py-4 whitespace-nowrap text-center text-sm font-medium">
+                      <a href="/edit" class="text-indigo-600 hover:text-indigo-900">
+                      Edit <br />
+                        Delete
+                      </a>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td class="px-6 py-4 whitespace-nowrap">
+                      <div class="flex items-center">
+                        <div class="flex-shrink-0">875</div>
+                      </div>
+                    </td>
+                    <td class="px-6 py-4 whitespace-nowrap">
+                      <div class="text-sm text-gray-500">RVD</div>
+                    </td>
+                    <td class="px-6 py-4 whitespace-nowrap">
+                      <div class="text-sm text-gray-500">
+                        --/--
+                      </div>
+                    </td>
+                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                    <button type="button" class="inline-flex items-center px-3 py-2 border border-gray-300 shadow-sm text-sm leading-4 font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                      Set as Default
+                    </button>
+                    </td>
+                    <td class="px-6 py-4 whitespace-nowrap text-center text-sm font-medium">
+                      <a href="/edit" class="text-indigo-600 hover:text-indigo-900">
+                      Edit <br />
+                        Delete
+                      </a>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td class="px-6 py-4 whitespace-nowrap">
+                      <div class="flex items-center">
+                        <div class="flex-shrink-0">688</div>
+                      </div>
+                    </td>
+                    <td class="px-6 py-4 whitespace-nowrap">
+                      <div class="text-sm text-gray-500">RCS</div>
+                    </td>
+                    <td class="px-6 py-4 whitespace-nowrap">
+                      <div class="text-sm text-gray-500">
+                        --/--
+                      </div>
+                    </td>
+                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                    <button type="button" class="inline-flex items-center px-3 py-2 border border-gray-300 shadow-sm text-sm leading-4 font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                      Set as Default
+                    </button>
+                    </td>
+                    <td class="px-6 py-4 whitespace-nowrap text-center text-sm font-medium">
+                      <a href="/edit" class="text-indigo-600 hover:text-indigo-900">
+                      Edit <br />
+                        Delete
+                      </a>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td class="px-6 py-4 whitespace-nowrap">
+                      <div class="flex items-center">
+                        <div class="flex-shrink-0">335</div>
+                      </div>
+                    </td>
+                    <td class="px-6 py-4 whitespace-nowrap">
+                      <div class="text-sm text-gray-500">QWF</div>
+                    </td>
+                    <td class="px-6 py-4 whitespace-nowrap">
+                      <div class="text-sm text-gray-500">
+                        --/--
+                      </div>
+                    </td>
+                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                    <button type="button" class="inline-flex items-center px-3 py-2 border border-gray-300 shadow-sm text-sm leading-4 font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                      Set as Default
+                    </button>
+                    </td>
+                    <td class="px-6 py-4 whitespace-nowrap text-center text-sm font-medium">
+                      <a href="/edit" class="text-indigo-600 hover:text-indigo-900">
+                        Edit <br />
+                        Delete
+                      </a>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
     </div>
   );
 };
