832e4c833c6fd3ab10806fd8d3914e9c2acf0453
DeeAnnEye <danielashulamithjustin@gmail.com>
2021-11-14 20:32:50 +0530

    Add chart

---
+++ src/components/LineChart.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { Line } from 'react-chartjs-2';
+
+const data = {
+    labels: ["A", "B", "C", "D", "E", "F"],
+            datasets: [{
+                label: "Date",
+                backgroundColor: "rgba(247, 163, 58, 0.1)",
+                borderColor: '#fd9d24',
+                data: [23,45,34,22,30,43],
+                lineTension: 0.1,
+            }]
+};
+
+const options = {
+    plugins: {
+    legend: {
+        display: false
+    },
+    // animation: {
+    //     easing: "easeInOutBack"
+    // },
+    scales: {
+        x: {
+            grid:{
+             display:false,
+             lineWidth: 0
+                 }
+           },
+        y: 
+           {
+         grid:{
+          display:false,
+          lineWidth: 0
+              }
+           }
+               }
+}
+};
+
+const LineChart = () => {
+ return(
+    <div class="w-full p-16">
+    <Line data={data} options={options} />
+    </div>
+ );
+}
+
+export default LineChart
--- src/pages/Billing.js
+++ src/pages/Billing.js
@@ -1,21 +1,23 @@
 import React from "react";
 import BillingInvoice from "../components/BillingInvoice";
 import BillingSubscription from "../components/BillingSubscription";
-import AdvertiserUserTable from "../components/AdvertiserUserTable";
+// import AdvertiserUserTable from "../components/AdvertiserUserTable";
 import PageBar from "../components/PageBar";
 import BillingAction from "../components/BillingAction";
+import LineChart from "../components/LineChart";
 
 const Billing = () => {
   return (
     <div class="min-h-full">
       <PageBar />
-      <div className=" grid grid-cols-2 px-4 py-4 gap-4 bg-white shadow">
+      <div className=" grid grid-cols-2 px-4 py-4 gap-4 mb-10 bg-white shadow">
         <div className="flex flex-col">
           <BillingSubscription />
           <BillingInvoice />
         </div>
         <BillingAction />
       </div>
+      <LineChart />     
     </div>
   );
 };
