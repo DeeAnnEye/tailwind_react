c020c778d7229500d5ede88dfc5de61716252348
DeeAnnEye <danielashulamithjustin@gmail.com>
2021-11-20 21:25:48 +0530

    Add offer assets

--- src/pages/OfferList.js
+++ src/pages/OfferList.js
@@ -2,19 +2,29 @@ import React from "react";
 import NavBar from "../components/NavBar";
 import AdvertiserPageBar from "../components/AdvertiserPageBar";
 import OfferListOptions from "../components/OfferListOptions";
-import OfferListPGB from "../components/OfferListPGB"
+import OfferListPGB from "../components/OfferListPGB";
 import OfferListTableA from "../components/OfferListTableA";
 import AdvertiserPagination from "../components/AdvertiserPagination";
+import OfferAssetsPGB from "../components/OfferAssetsPGB";
 
-const OfferList = () =>{
-    return <div class="h-full">
-    <NavBar />
-    <AdvertiserPageBar />
-    <OfferListPGB />
-    <OfferListOptions />
-    <OfferListTableA />
-    <AdvertiserPagination />
+const OfferList = () => {
+  return (
+    <div class="h-full">
+      <div className="pb-20">
+        <NavBar />
+        <AdvertiserPageBar />
+        <OfferListPGB />
+        <OfferListOptions />
+        <OfferListTableA />
+        <AdvertiserPagination />
+      </div>
+      <div className="pb-20">
+        <NavBar />
+        <AdvertiserPageBar />
+        <OfferAssetsPGB />
+      </div>
     </div>
-}
+  );
+};
 
-export default OfferList
+export default OfferList;
